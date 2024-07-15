83b43458ae0afe1a192455b25399f6cd6704c1f3
woojin <woojin.seo@icloud.com>
2024-07-15 14:41:19 -0500

    feature: format updated

--- src/components/CaclulateResult.tsx
+++ src/components/CaclulateResult.tsx
@@ -1,24 +1,19 @@
 import { Card, CardBody } from "@nextui-org/react";
 
 type Prop = {
-  answer: string
-  postfix: string
-}
+  answer: string;
+  postfix: string;
+};
 
-export const CalculateResult = ({answer, postfix}: Prop) => {
-
+export const CalculateResult = ({ answer, postfix }: Prop) => {
   return (
     <div className="flex place-self-center flex-col gap-4">
       <Card>
-        <CardBody>
-          {`Answer: ${answer}`}
-        </CardBody>
+        <CardBody>{`Answer: ${answer}`}</CardBody>
       </Card>
       <Card>
-        <CardBody>
-          {`Postfix: ${postfix}`}
-        </CardBody>
+        <CardBody>{`Postfix: ${postfix}`}</CardBody>
       </Card>
     </div>
-  )
-}
+  );
+};
--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -1,8 +1,9 @@
-
 export const TitleBar = () => {
   return (
     <div className="flex items-center mt-10 w-96">
-      <h1 className="text-3xl w-96 border-2 text-center p-5">Simple Text Calculator</h1>
+      <h1 className="text-3xl w-96 border-2 text-center p-5">
+        Simple Text Calculator
+      </h1>
     </div>
   );
 };
