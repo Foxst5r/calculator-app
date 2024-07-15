913366cefea165b9dd117db07aebbf0f44531b71
woojin <woojin.seo@icloud.com>
2024-07-15 14:35:19 -0500

    feature: Added titlebar, calculate input and returning results ui

---
+++ src/components/CaclulateResult.tsx
@@ -0,0 +1,24 @@
+import { Card, CardBody } from "@nextui-org/react";
+
+type Prop = {
+  answer: string
+  postfix: string
+}
+
+export const CalculateResult = ({answer, postfix}: Prop) => {
+
+  return (
+    <div className="flex place-self-center flex-col gap-4">
+      <Card>
+        <CardBody>
+          {`Answer: ${answer}`}
+        </CardBody>
+      </Card>
+      <Card>
+        <CardBody>
+          {`Postfix: ${postfix}`}
+        </CardBody>
+      </Card>
+    </div>
+  )
+}
